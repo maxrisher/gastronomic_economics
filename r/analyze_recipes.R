a475f7331d3efa8b24ffe95026f29e328ee0eb25
Max Risher <max.risher@gmail.com>
2024-11-09 18:35:51 -0800

    completed manual correction of food mapping errors

--- r/analyze_recipes.R
+++ r/analyze_recipes.R
@@ -19,6 +19,9 @@ ingredients_to_kroger <-
 recipe_descriptions <- 
   read_csv("data/all_budget_bytes_recipes.csv")
 
+animal_products <- 
+  read_csv("data/tagged_veggie_recipes.csv")
+
 # Full ingredients clean data ---------------------------------------------------
 
 budget_bytes_ingredients <- 
@@ -50,43 +53,198 @@ ingredients_clean <-
          recipe_ingredient_price = as.double(recipe_ingredient_price),
          price = case_when(
            recipe_ingredient_price == 0 ~ 0,
-           TRUE ~ product_local_price
-         ))
-#vegetarian = if_else(grepl("Meat & Seafood", product_categories), FALSE, TRUE)
+           TRUE ~ product_local_price))
 
-# Make sure ingredients data are correct ----------------------------------
+# Make sure product data is correct ----------------------------------
 error_detect <- 
   ingredients_clean %>% 
   filter(!is.na(kroger_upc)) %>% 
   select(product_name, claude_product_grams_estimate, product_sold_by_unit) %>% 
   distinct() %>% 
-  mutate(claude_ounces = claude_product_grams_estimate * 0.035274) %>% 
-  mutate(fl_oz = grepl("fl oz", product_sold_by_unit),
-         gal = grepl("gal", product_sold_by_unit),
+  mutate(claude_ounces = claude_product_grams_estimate * 0.035274) %>% # convert claude's estimate to grams
+  mutate(fl_oz = grepl("fl oz", product_sold_by_unit), # find fl_oz measures
+         gal = grepl("gal", product_sold_by_unit), # gal
          pound = grepl("pound", product_sold_by_unit),
          lb = grepl("lb", product_sold_by_unit),
          count = grepl("ct", product_sold_by_unit),
          ounces = grepl("oz", product_sold_by_unit)) %>% 
-  mutate(unknown = !lb & !gal & !count & !ounces & !pound & !fl_oz,
-         only_ounces = ounces & !count & !lb & !gal & !pound & !fl_oz,
+  mutate(unknown = !lb & !gal & !count & !ounces & !pound & !fl_oz, 
+         only_ounces = ounces & !count & !lb & !gal & !pound & !fl_oz, 
          only_count = count & !lb & !gal & !ounces & !pound & !fl_oz,
-         very_tough = only_count | unknown,
-         tough = very_tough | gal | fl_oz)
+         very_tough = only_count | unknown, #These are products with no explicit mass or volume
+         tough = very_tough | gal | fl_oz) #These are products without explicit mass
+
+error_detect %>% 
+  filter(tough == TRUE) %>% 
+  select(product_name, claude_product_grams_estimate, product_sold_by_unit, claude_ounces) %>% 
+  arrange(desc(claude_ounces)) %>% 
+  print(n=500)
+
+#pineapple, cucumber, and corn are inconsistent
+
+#arrange the ingredients from estimated ounces high to low and check for issues
+error_detect %>% 
+  filter(ounces == TRUE) %>% 
+  select(product_name, claude_product_grams_estimate, product_sold_by_unit, claude_ounces) %>% 
+  arrange(desc(claude_ounces)) %>% 
+  view()
+
+error_detect %>% 
+  filter(ounces == FALSE) %>% 
+  select(product_name, claude_product_grams_estimate, product_sold_by_unit, claude_ounces) %>% 
+  arrange(desc(claude_ounces)) %>% 
+  view()
 
 # This item seems to be wrong: private-selection-heritage-boneless-pork-shoulder-roast-natural-duroc-pork
-# ounces it seems to get right
-# just arrange them from estimated ounces high to low and check the descriptions
+
+# Make sure full ingredients data is correct ------------------------------
+ingredients_clean %>% 
+  mutate(product_price_per_g = price / claude_product_grams_estimate,
+         ingredient_price = product_price_per_g * claude_ingredient_gram_estimate,
+         bb_underspend = ingredient_price - recipe_ingredient_price) %>% 
+  view()
 
 # Create final ingredients dataset ----------------------------------------
+
+fixed_grains_mass <- 
+  bb_ingredients_query_grams %>%
+  mutate(ingredient_gram_estimate = case_when(
+    # Match cooked (but not uncooked) rice, excluding bulgur
+    str_detect(recipe_ingredient_description, "(?<!un)cooked") &
+      str_detect(recipe_ingredient_description, "rice") &
+      !str_detect(recipe_ingredient_description, "bulgur") ~ claude_ingredient_gram_estimate/2.5,
+    
+    # Match cooked (but not uncooked) bulgur
+    str_detect(recipe_ingredient_description, "(?<!un)cooked") &
+      str_detect(claude_query_for_kroger, "bulgur") ~ claude_ingredient_gram_estimate/4.1,
+    
+    # Match cooked (but not uncooked) farro
+    str_detect(recipe_ingredient_description, "(?<!un)cooked") &
+      str_detect(claude_query_for_kroger, "farro") ~ claude_ingredient_gram_estimate/3,
+    
+    # Match cooked (but not uncooked) quinoa
+    str_detect(recipe_ingredient_description, "(?<!un)cooked") &
+      str_detect(claude_query_for_kroger, "quinoa") ~ claude_ingredient_gram_estimate/3.1,    
+    
+    # Match cooked (but not uncooked) mashed potatoes
+    str_detect(claude_query_for_kroger, "mashed potatoes") ~ claude_ingredient_gram_estimate/5,
+    
+    # Match cooked (but not uncooked) lentils
+    str_detect(recipe_ingredient_description, "(?<!un)cooked") &
+      str_detect(claude_query_for_kroger, "lentil") ~ claude_ingredient_gram_estimate/3.1,
+    
+    # All other cases remain unchanged
+    TRUE ~ claude_ingredient_gram_estimate
+  ))
+
+
+kroger_product_data <- 
+  ingredients_to_kroger %>% 
+  select(kroger_upc, product_grams, product_uri, product_local_price, product_categories, product_sold_by_unit) %>% 
+  group_by(kroger_upc) %>% 
+  summarise(product_grams = mean(product_grams),
+            product_uri = first(product_uri),
+            product_local_price = first(product_local_price),
+            product_categories = first(product_categories),
+            product_sold_by_unit = first(product_sold_by_unit)) %>% 
+  distinct()
+
+corrected_kroger_upc <- 
+  ingredients_to_kroger %>% 
+  mutate(fixed_kroger_upc = case_when(
+    
+    # Convert old fashioned bobs red mill oats to kroger oats
+    str_detect(kroger_upc, "3997805155") ~ "1111076655.0",
+    
+    # Convert bobs red mill oats to kroger oats
+    str_detect(kroger_upc, "3997805155") ~ "1111076655.0",
+    
+    # Convert ottogi cooked rice pack to dry rice
+    str_detect(kroger_upc, "64517589041") ~ "1111084706.0",
+    
+    # Convert kroger cooked rice pack to dry rice
+    str_detect(kroger_upc, "1111001950") ~ "1111087415.0",
+    
+    # Convert boiled eggs to eggs
+    str_detect(kroger_upc, "74602520246") ~ "1111085724.0",
+    
+    # Ham hocks are NA
+    str_detect(food_name, "smoked ham hocks") ~ "21386300000.0",
+    
+    # Chicken pieces should not be fried and frozen
+    str_detect(kroger_upc, "3100011611.0") ~ "28334750000.0",
+
+    # All other cases remain unchanged
+    TRUE ~ kroger_upc
+  )) %>% 
+  select(food_name, query, alternate_query_inbox, fixed_kroger_upc)
+  
+products_remapped <- 
+  corrected_kroger_upc %>% 
+  left_join(kroger_product_data, by = join_by(fixed_kroger_upc == kroger_upc))
+
+kroger_corrected <- 
+  products_remapped %>% 
+  mutate(product_grams = case_when(product_uri == "/p/private-selection-heritage-boneless-pork-shoulder-roast-natural-duroc-pork/0021358400000?cid=dis.api.tpi_products-api_20240521_b:all_c:p_t:thalosfoodresearch-f" ~ 453.59200,
+                                                   .default = product_grams))
+
+# Merge up ----------------------------------------------------------------
+merged_ingredients <- 
+  fixed_grains_mass %>% 
+  left_join(kroger_corrected, by = join_by(claude_query_for_kroger == food_name)) %>% 
+  rename(kroger_query = query,
+         claude_product_grams_estimate = product_grams)
+
+ingredients_final <- 
+  merged_ingredients %>% 
+  mutate(product_name = str_extract(product_uri, "/p/([^/]+)") %>% str_remove("/p/"),
+         kroger_upc = str_extract(product_uri, "\\d+(?=\\?)"),
+         recipe_ingredient_price = gsub("\\$", "", recipe_ingredient_price),
+         recipe_ingredient_price = as.double(recipe_ingredient_price),
+         price = case_when(
+           recipe_ingredient_price == 0 ~ 0,
+           TRUE ~ product_local_price)) %>% 
+  mutate(product_price_per_g = price / claude_product_grams_estimate,
+         ingredient_price = product_price_per_g * claude_ingredient_gram_estimate,
+         bb_underspend = ingredient_price - recipe_ingredient_price)
+
+
+
+
 
 ingredients_final <- 
   ingredients_clean %>% 
   # Fix a clearly incorrect sell_by_unit from count to lb.
-  mutate(claude_product_grams_estimate = case_when(product_name == "private-selection-heritage-boneless-pork-shoulder-roast-natural-duroc-pork" ~ 453.59200,
-                                                   .default = claude_product_grams_estimate)) %>% 
+  # mutate(claude_product_grams_estimate = case_when(product_name == "private-selection-heritage-boneless-pork-shoulder-roast-natural-duroc-pork" ~ 453.59200,
+  #                                                  .default = claude_product_grams_estimate)) %>% 
   mutate(product_price_per_g = price / claude_product_grams_estimate,
          ingredient_price = product_price_per_g * claude_ingredient_gram_estimate,
          bb_underspend = ingredient_price - recipe_ingredient_price)
+
+ingredients_final %>% 
+  select(recipe_ingredient_description, product_name) %>% 
+  distinct() %>% 
+  view()
+
+ingredients_final %>% 
+  select(recipe_ingredient_description, product_name) %>% 
+  distinct() %>% 
+  slice(3501:4000) %>% 
+  print(n=501)
+
+ingredients_final %>% 
+  select(kroger_query) %>% 
+  distinct() %>% 
+  view()
+
+ingredients_final %>% 
+  write_csv('for_manual_inspection.csv')
+
+#row 38
+#rolled oats
+#cooked rice: Ottogi-Cooked-Rice
+#bacon strips
+#mashed potatoes
 
 # Detect anomalies --------------------------------------------------------
 
@@ -158,5 +316,5 @@ ingredients_to_kroger %>%
 
 # Further -----------------------------------------------------------------
 
-Substitute beyond beef and impossible beef for ground beef and see what happens
+#Substitute beyond beef and impossible beef for ground beef and see what happens
 
